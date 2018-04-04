ed04703c32a80b94a9635f7b37ac0d2a79f7f9a2
strider90arun <strider90arun@gmail.com>
2018-04-04 08:59:33 -0700

    Message object

---
+++ src/main/java/com/messaging/message/Message.java
@@ -0,0 +1,29 @@
+package com.messaging.message;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+/**
+ * Immutable class representing a message and type.
+ */
+public final class Message implements Serializable {
+
+    private final byte[] message;
+    private final MessageType type;
+
+    @JsonCreator
+    public Message(@JsonProperty("message") byte[] message, @JsonProperty("type") MessageType type) {
+        this.message = message;
+        this.type = type;
+    }
+
+    public byte[] getMessage() {
+        return message;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+}
